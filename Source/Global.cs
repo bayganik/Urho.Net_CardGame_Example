8e86ebc97f34572c15453a33f354386bac0aabb1
Kamran Baygani <bayganik@gmail.com>
2021-11-19 11:28:35 -0500

    update of NodeProcessingSystem

--- Source/Global.cs
+++ Source/Global.cs
@@ -9,35 +9,7 @@ using Urho.Urho2D;
 
 namespace CardGameExample
 {
-    public class NodeProcessingSystem
-    {
-        /*
-         * The system part of ECS framework. Executed on every update frame
-         * Allows for processing of Nodes outside of Scene component for separation of concerns
-         * 
-         * You can use the Tag property to group Nodes together or use Component to group them
-         * See the Process method
-         */
-        public SceneComponent Scene;
-        public Node[] Nodes;
-        public string Tag;
-        public bool Recursive;
-        public NodeProcessingSystem(SceneComponent _scene, string _tag, bool _recursive = false)
-        {
-            Tag = _tag;
-            Scene = _scene;
-            Recursive = _recursive;
-        }
-        public virtual void Process()
-        {
-            //Nodes = Scene.MyScene.GetChildrenWithComponent<CardInfo>();
-            //Nodes = Scene.MyScene.GetChildrenWithTag(Tag, Recursive);
-            //foreach(Node nd in Nodes)
-            //{
 
-            //}
-        }
-    }
     public class mmWindow : Window
     {
         /*
